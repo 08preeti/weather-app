de550b6d64c8d6ff92c8413e04f2afa0be13ff89
08preeti <chikkalipreeti08@gmail.com>
2026-01-09 00:51:40 +0530

    Added JavaScript logic for user input and button click

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Weather App Started");
+const button = document.getElementById("getWeatherBtn");
+const cityInput = document.getElementById("cityInput");
+const weatherResult = document.getElementById("weatherResult");
+
+button.addEventListener("click", function () {
+    const city = cityInput.value;
+
+    if (city === "") {
+        weatherResult.innerHTML = "<p>Please enter a city name</p>";
+        return;
+    }
+
+    weatherResult.innerHTML = `
+        <p>City: ${city}</p>
+        <p>Fetching weather data...</p>
+    `;
+});
+
